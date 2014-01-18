b0055dfcd92e3334fb9498277c4a79f62711aae3
garwedgess <garethwilliams21@gmail.com>
2014-01-18 21:20:34 +0530

    [1/4] Base: Breathing missedcall/sms/voicemail
    Adds a breathing effect to missed call/SMS/voicemail notifications.
    
    Mms: https://gerrit.omnirom.org/#/c/4977/
    OmniGears: https://gerrit.omnirom.org/#/c/4978/
    Telephony: https://gerrit.omnirom.org/#/c/4979/
    
    Change-Id: I1b7f40c2fc5c6612303cf6575e3efe8e992cecae
    
    @alviteri: Port by me to crDroid Android
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3714,6 +3714,24 @@ public final class Settings {
          */
         public static final String TOAST_ANIMATION = "toast_animation";
 
+        /**
+         * Give SMS notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+         * Give Missed call notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+        /**
+         * Give Voicemail notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
+
         /**  
          * AOKP Custom System Animations
          * @hide
